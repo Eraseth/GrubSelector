376794c5d9d6bac28765e4bdee7e2b7d16aaa4b3
WISSLE <maxime.wissle@gmail.com>
2017-03-09 17:52:57 +0100

    début d'automatisation

--- startOs.sh
+++ startOs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export SUDO_ASKPASS="`dirname $0`/myaskpass.sh"
 
+listOs=`grep -i "menuentry " /boot/grub/grub.cfg`
+echo $listOs
 os=`zenity --list --title "Select an OS" --column="Number (GRUB)" --column="Name" --column="Description" 0 "Ubuntu16" "Take control with Ubuntu" 1 "Windows10" "Windows"`
 if [ "$os" != "" ]
 then
